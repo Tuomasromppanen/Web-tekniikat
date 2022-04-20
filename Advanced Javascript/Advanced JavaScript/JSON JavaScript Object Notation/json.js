9d15b513f5b9cc423a36f6cd6a50d7a8500dd9b1
Tuomasromppanen <tuomas.romppanen@outlook.com>
2022-04-20 10:02:48 +0300

    commit

--- Advanced Javascript/Advanced JavaScript/JSON JavaScript Object Notation/json.js
+++
@@ -1,5 +0,0 @@
-//Handling the contents:JSON
-let text = '{"employees": ['+
-'{"firstname":"John", "lastname":"Doe"},' +
-'{"firstname":"Anna", "lastname":"Smith"},' +
-'{"firstname":"Peter", "lastname":"Jones"} ]}';
